49604d3d80284a4f639672f543459984e93c6362
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-03-19 23:56:33 +0000

    feat: add ProductImage with is_primary property

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    //
+    protected $fillable = ['path', 'product_id', 'is_primary'];
+
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
