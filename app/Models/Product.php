c1f69e70816e408702868e13a0d166a12697814c
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-03-19 23:56:24 +0000

    feat: add Product with slug, category and images relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
-    //
+    use HasSlug;
+
+    protected $fillable = ['name', 'slug', 'description', 'price', 'category_id'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
