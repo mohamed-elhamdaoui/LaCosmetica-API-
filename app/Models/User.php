6b778d943dda29297c8fc8042db6b8572ecf5691
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-03-20 00:05:40 +0000

    feat: update User with roles and JWT interface

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,10 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -18,11 +20,15 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name', 'email', 'password', 'role'];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function getJWTIdentifier() { return $this->getKey(); }
+    public function getJWTCustomClaims() { return []; }
 
     /**
      * The attributes that should be hidden for serialization.
